72504511630c10fc756a9862fb43cd33cc6b6bee
Jean-Baptiste Roquefere <jeanbaptiste.roquefere@gmail.com>
2019-04-30 13:52:33 +0200

    wizard works

--- groupe_cayla/models/tournee_vt.py
+++ groupe_cayla/models/tournee_vt.py
@@ -11,13 +11,18 @@ class TourneeVT(models.Model):
     _description = 'Une tournée VT'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    client_id = fields.Many2one('groupe_cayla.client',required=True,ondelete='cascade')
     user_id = fields.Many2one(
         'res.users',
         delegate=False,
         required=True,
     )
 
-    vts = fields.One2many('groupe_cayla.planif_vt', 'tournee_vt_id', string='Visites')
+    vts = fields.Many2many('groupe_cayla.planif_vt', string='Visites')
 
-
+    @api.model
+    def default_get(self, fields_names):
+        defaults = super().default_get(fields_names)
+        client_ids = self.env.context['active_ids']
+        defaults['user_id'] = self.env.context['uid']
+        defaults['vts'] = [p.id for p in self.env['groupe_cayla.planif_vt'].search([('client_id', 'in', client_ids)])]
+        return defaults
