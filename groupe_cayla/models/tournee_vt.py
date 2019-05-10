3f75118ce07aac9602518e5ece3e9df23e9adb8e
Jean-Baptiste Roquefere <jeanbaptiste.roquefere@gmail.com>
2019-05-10 11:26:43 +0200

    date et utilisateur,

--- groupe_cayla/models/tournee_vt.py
+++ groupe_cayla/models/tournee_vt.py
@@ -17,12 +17,22 @@ class TourneeVT(models.Model):
         required=True,
     )
 
+    technicien_id = fields.Many2one('res.users',compute='_compute_tournee')
+    date_tournee = fields.Date(compute='_compute_tournee')
     vts = fields.Many2many('groupe_cayla.planif_vt', string='Visites')
+
+    @api.depends('vts')
+    def _compute_tournee(self):
+        for d in self:
+            if d.vts and len(d.vts) > 0:
+                d.technicien_id = d.vts[0].technicien_id
+                d.date_tournee = d.vts[0].date_time_planif.date()
 
     @api.model
     def default_get(self, fields_names):
         defaults = super().default_get(fields_names)
-        client_ids = self.env.context['active_ids']
         defaults['user_id'] = self.env.context['uid']
-        defaults['vts'] = [p.id for p in self.env['groupe_cayla.planif_vt'].search([('client_id', 'in', client_ids)])]
+        if 'active_ids' in self.env.context:
+            client_ids = self.env.context['active_ids']
+            defaults['vts'] = [p.id for p in self.env['groupe_cayla.planif_vt'].search([('client_id', 'in', client_ids)])]
         return defaults
